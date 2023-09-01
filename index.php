a22ca0ca73a263648a39067f44ce9fbe806783cc
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-09-01 18:03:26 -0300

    tratando exception

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=php_pdo'; //data source name
+$dsn = 'mysql:host=localhost;dbname=pdhp_pdo'; //data source name
 $user = 'root';
 $senha = '';
-$conexao = new PDO($dsn, $user, $senha);
+
+try {
+    $conexao = new PDO($dsn, $user, $senha);
+} catch (PDOException $e) {
+    echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
+    //registrar erro
+}
 ?>
