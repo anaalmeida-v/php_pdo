4b087732bd3bbe89df9a5c3d8ab17b3a2ff07b25
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-09-04 17:34:43 -0300

    Listando registros com Foreach

--- index.php
+++ index.php
@@ -8,17 +8,26 @@ try {
     $conexao = new PDO($dsn, $user, $senha);
 
     $query = '
-        select * from tb_usuarios order by nome desc limit 1
+        select * from tb_usuarios
     ';
 
-    $stmt = $conexao->query($query); //stmt - statement //query - retorna um pdo statement
-    $usuario = $stmt->fetch(PDO::FETCH_ASSOC); //retorna todos os registros retornados da consulta
+    //$stmt = $conexao->query($query); //stmt - statement //query - retorna um pdo statement
+    foreach($conexao->query($query) as $chave => $valor){
+        echo '<pre>';
+        print_r($valor['nome']);
+        echo '<hr>';
+        echo '</pre>';
+    }
+    //$lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC); //retorna todos os registros retornados da consulta
 
-    echo '<pre>';
-    print_r($usuario);
-    echo '</pre>';
+    /*echo '<pre>';
+    print_r($lista_usuarios);
+    echo '</pre>';*/
 
-    echo $usuario['nome'];
+    /*foreach ($lista_usuarios as $key => $value) {
+        echo $value['nome'];
+        echo '<hr>';
+    }*/
 
 } catch (PDOException $e) {
     echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
