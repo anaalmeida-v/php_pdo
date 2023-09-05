655539110a9b2811f9d3392894a3bf7b69af3875
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-09-05 17:16:33 -0300

    prepare statement

--- index.php
+++ index.php
@@ -4,21 +4,25 @@
         $senha = '';
         try {
             $conexao = new PDO($dsn, $usuario, $senha);
-            //query
-            $query = "select * from tb_usuarios where ";
-            $query .= " email = '{$_POST['usuario']}'"; //atribuindo e concatenando com o que já existe na variável query
-            $query .= " AND senha = '{$_POST['senha']}'"; //utilizando chaves para que não haja conflitos por conta das aspas
-            echo $query;
 
-            //$stmt = $conexao->query($query); //instância do método PDO, executando método query, passando nossa query
-            //$usuario = $stmt->fetch(); //recupera usuario através de statement executando o método fetch
-            echo '<hr>';
+            $query = "select * from tb_usuarios where ";
+            $query .= " email = :usuario ";
+            $query .= " AND senha = :senha";
+            $stmt = $conexao->prepare($query); //como o método query retorna um PDOStatement, porém ele não executa a query diretamente, fica aguardando que ordem seja dada
+            $stmt->bindValue(':usuario', $_POST['usuario']); //bindValue recebe: ('variável de ligação(bind)' + valor da ligação)
+            $stmt->bindValue(':senha', $_POST['senha'], PDO::PARAM_INT);
+
+            $stmt->execute(); //executando método
+    
+            $usuario = $stmt->fetch(); //recuperar 1º registro retornado com a consulta
+    
             echo '<pre>';
-            //print_r($usuario);
+            print_r($usuario);
             echo '</pre>';
+
         } catch (PDOException $e) {
             echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
-            //registrar o erro de alguma forma.S
+            //registrar o erro de alguma forma.
         }
     }
     ?>
