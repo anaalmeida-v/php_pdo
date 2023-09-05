117cab249eab8ca4822dc11cc8d0c9f078e95143
aninhaalmeidav <aninhaalmeidav@gmail.com>
2023-09-05 15:52:02 -0300

    sql injection

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
-<?php
-if (!empty($_POST['usuario']) && !empty($_POST['senha'])) { //imprimindo superglobal post
-    $dsn = 'mysql:host=localhost;dbname=php_pdo'; //data source name
-    $user = 'root';
-    $senha = '';
+<?php if (!empty($_POST['usuario']) && !empty($_POST['senha'])) { //só entra na lógica se índices 'usuario' e 'senha' da super-global POST não estiverem vazos
+        $dsn = 'mysql:host=localhost;dbname=php_pdo'; //data source name
+        $usuario = 'root';
+        $senha = '';
+        try {
+            $conexao = new PDO($dsn, $usuario, $senha);
+            //query
+            $query = "select * from tb_usuarios where ";
+            $query .= " email = '{$_POST['usuario']}'"; //atribuindo e concatenando com o que já existe na variável query
+            $query .= " AND senha = '{$_POST['senha']}'"; //utilizando chaves para que não haja conflitos por conta das aspas
+            echo $query;
 
-    try {
-        $conexao = new PDO($dsn, $usuario, $senha);
-
-        //query
-        $query = "select * from tb_usuarios where";
-        $query .= "email = '{$_POST['usuario']}'";
-        $query .= "AND senha = '{$_POST['senha']}'";
-        echo $query;
-
-        $stmt = $conexao->query($query);
-        $usuario = $stmt->fetch();
-        print_r($usuario);
-
-    } catch (PDOException $e) {
-        echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
+            //$stmt = $conexao->query($query); //instância do método PDO, executando método query, passando nossa query
+            //$usuario = $stmt->fetch(); //recupera usuario através de statement executando o método fetch
+            echo '<hr>';
+            echo '<pre>';
+            //print_r($usuario);
+            echo '</pre>';
+        } catch (PDOException $e) {
+            echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
+            //registrar o erro de alguma forma.S
+        }
     }
-}
-
-?>
-
+    ?>
 <html>
 
 <head>
@@ -32,11 +30,11 @@ if (!empty($_POST['usuario']) && !empty($_POST['senha'])) { //imprimindo supergl
 </head>
 
 <body>
-    <form method="post" action="index.php"><!--método + local onde ação será feita-->
-        <input type="text" placeholder="usuário" name="usuario">
-        <br>
-        <input type="password" placeholder="senha" name="senha">
-        <br>
+    <form method="post" action="index.php">
+        <input type="text" placeholder="usuário" name="usuario" />
+        <br />
+        <input type="password" placeholder="senha" name="senha" />
+        <br />
         <button type="submit">Entrar</button>
     </form>
 </body>
